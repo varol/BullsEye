2ae4e229ba85d17e02850f0006a8424c96a165a2
Varol AKSOY <varolaksoy@gmail.com>
2019-08-17 10:56:30 +0300

    Initial Commit

---
+++ BullEyes/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  BullEyes
+//
+//  Created by VAROL on 17.08.2019.
+//  Copyright © 2019 VAROL AKSOY. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
